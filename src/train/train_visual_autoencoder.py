fedc55596d018cac0199cc54cb232ad7009be8f5
scrannah <hannah.timms@hotmal.co.uk>
2025-12-05 15:25:15 +0000

    visual ae training

--- src/train/train_visual_autoencoder.py
+++ src/train/train_visual_autoencoder.py
@@ -1,20 +1,23 @@
+
 def train_visual_autoencoder(
         model,
-        dataloader,
+        train_dataloader,
         optimizer,
-        criterion,
+        criterion_images,
         device,
         num_epochs
     ):
 
-    model.train()
     epoch_losses = []
+
+    model.train()
+
 
     for epoch in range(num_epochs):
 
         running_loss = 0.0
 
-        for images in dataloader:
+        for images in train_dataloader:
 
             # Move batch of images to device
             images = images.to(device)
@@ -23,7 +26,7 @@ def train_visual_autoencoder(
             x_content, _ = model(images)
 
             # Compute reconstruction loss, how does it compare to the actual image
-            loss = criterion(x_content, images) # I ONLY WANT CONTENT FOR VISUAL TRAINING, THIS TAKES CONTENT FROM THE DECODER ONLY
+            loss = criterion_images(x_content, images) # I ONLY WANT CONTENT FOR VISUAL TRAINING, THIS TAKES CONTENT FROM THE DECODER ONLY
 
             # Backpropagation
             optimizer.zero_grad()
@@ -32,8 +35,13 @@ def train_visual_autoencoder(
 
             running_loss += loss.item() * images.size(0)
 
-        epoch_loss = running_loss / len(dataloader.dataset)
+        epoch_loss = running_loss / len(train_dataloader.dataset)
+        epoch_losses.append(epoch_loss)
+        print(f"[Epoch {epoch+1}] AE Loss: {epoch_loss:.4f}")
+
+
+        epoch_loss = running_loss / len(train_dataloader.dataset)
         epoch_losses.append(epoch_loss)
         print(f"[Epoch {epoch+1}] AE Loss: {epoch_loss:.4f}") # this needs fixing to display correct epoch
 
-    return epoch_losses
+        return epoch_losses
--- src/utils/training_utils.py
+++ src/utils/training_utils.py
@@ -8,6 +8,7 @@ from .token_generate import generate
 from torchmetrics.image import StructuralSimilarityIndexMeasure
 from nltk.translate.bleu_score import sentence_bleu
 
+
 def init_weights(m):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
         nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
@@ -19,155 +20,151 @@ def init_weights(m):
 
 
 # Plots four images and their reconstructions
-def validation( model, data_loader, device, tokenizer, criterion_images, criterion_text ):
-  model.eval()
-  with torch.no_grad():
-    frames, descriptions, image_target, text_target = next(iter(data_loader))
+def validation(model, data_loader, device, tokenizer, criterion_ctx, criterion_text):
+    model.eval()
+    with torch.no_grad():
+        frames, descriptions, image_target, text_target = next(iter(data_loader))
 
-    descriptions = descriptions.to(device)
-    frames = frames.to(device)
-    image_target = image_target.to(device)
-    text_target = text_target.to(device)
+        descriptions = descriptions.to(device)
+        frames = frames.to(device)
+        image_target = image_target.to(device)
+        text_target = text_target.to(device)
 
-    ssim_fn = StructuralSimilarityIndexMeasure(data_range=1.0).to(device)
+        ssim_fn = StructuralSimilarityIndexMeasure(data_range=1.0).to(device)
 
-    # METRICS
+        # METRICS
 
-    predicted_image_k,context_image, predicted_text_logits_k, hidden, cell = model(frames, descriptions, text_target) # need all these for validation metrics
+        predicted_image_k, context_image, predicted_text_logits_k, hidden, cell = model(frames, descriptions,
+                                                                                        text_target)  # need all these for validation metrics
 
-    val_image_mse = criterion_images(predicted_image_k, image_target).item()
-    print(f"Validation Image MSE: {val_image_mse:.4f}")
+        val_image_mse = criterion_ctx(predicted_image_k, image_target).item()
+        print(f"Validation Image MSE: {val_image_mse:.4f}")
 
+        # flatten and remove teacher forcing
 
-    # flatten and remove teacher forcing
+        prediction_flat = predicted_text_logits_k.reshape(-1, tokenizer.vocab_size)
 
-    prediction_flat = predicted_text_logits_k.reshape(-1, tokenizer.vocab_size)
+        # flatten and remove teacher forcing
+        target_tokens = text_target.squeeze(1)[:, 1:]  # remove the dimension (1) and BOS
+        target_flat = target_tokens.reshape(-1)  # make it a 1d vector
 
-    # flatten and remove teacher forcing
-    target_tokens = text_target.squeeze(1)[:, 1:] #remove the dimension (1) and BOS
-    target_flat = target_tokens.reshape(-1) # make it a 1d vector
+        # calculating los for text
+        val_loss_text = criterion_text(prediction_flat, target_flat)
 
-    # calculating los for text
-    val_loss_text = criterion_text(prediction_flat, target_flat)
+        val_perplexity = torch.exp(val_loss_text).item()
+        print(f"Validation Perplexity: {val_perplexity:.2f}")
 
-    val_perplexity = torch.exp(val_loss_text).item()
-    print(f"Validation Perplexity: {val_perplexity:.2f}")
+        # SSIM
 
+        predicted_img = torch.clamp(predicted_image_k, 0, 1)  # SSIM needs values betwee 0,1 so we clamp for it here
+        target_img = torch.clamp(image_target, 0, 1)
+        ssim_val = ssim_fn(predicted_img, target_img).item()
+        print(f"Validation SSIM: {ssim_val:.4f}")
 
-    # SSIM
+        pred_ids = torch.argmax(predicted_text_logits_k, dim=-1)[0]
 
-    predicted_img = torch.clamp(predicted_image_k, 0, 1) #SSIM needs values betwee 0,1 so we clamp for it here
-    target_img  = torch.clamp(image_target, 0, 1)
-    ssim_val = ssim_fn(predicted_img, target_img).item()
-    print(f"Validation SSIM: {ssim_val:.4f}")
+        # Ground-truth IDs
+        gt_ids = text_target.squeeze(1)[0]  # get rid of middle dimension, pass first example for visualisation
 
+        # Decode tokens
+        pred_sentence = tokenizer.decode(pred_ids.tolist(), skip_special_tokens=True)
+        gt_sentence = tokenizer.decode(gt_ids.tolist(), skip_special_tokens=True)
 
+        val_bleu = sentence_bleu([gt_sentence.split()], pred_sentence.split())  # pass ground truth and predicted
+        print(f"Validation BLEU: {val_bleu:.4f}")
 
-    pred_ids = torch.argmax(predicted_text_logits_k, dim=-1)[0]
+        img_emb = model.image_encoder(predicted_image_k)
+        txt_emb = model.text_encoder(pred_ids.unsqueeze(0))
 
-    # Ground-truth IDs
-    gt_ids = text_target.squeeze(1)[0] #get rid of middle dimension, pass first example for visualisation
+        # if text encoder returns embrddinsg for each token. average them
+        if isinstance(txt_emb, tuple):  # if text encoder returns tuple not tensor, take 1st value only
+            txt_emb = txt_emb[0]
+        if txt_emb.dim() == 3:
+            txt_emb = txt_emb.mean(dim=1)  # mean pool sequence
 
-    # Decode tokens
-    pred_sentence = tokenizer.decode(pred_ids.tolist(), skip_special_tokens=True)
-    gt_sentence = tokenizer.decode(gt_ids.tolist(),  skip_special_tokens=True)
+        val_cross_modal = F.cosine_similarity(img_emb, txt_emb).mean().item()
+        print(f"Validation Cross-modal Similarity: {val_cross_modal:.4f}")
 
-    val_bleu = sentence_bleu([gt_sentence.split()], pred_sentence.split()) # pass ground truth and predicted
-    print(f"Validation BLEU: {val_bleu:.4f}")
+        figure, ax = plt.subplots(2, 6, figsize=(20, 5), gridspec_kw={'height_ratios': [2, 1.5]})
 
+        for i in range(4):
+            im = frames[0, i, :, :, :].cpu()
+            show_image(ax[0, i], im)
+            ax[0, i].set_aspect('auto')
+            ax[0, i].axis('off')
+            wrapped_text = textwrap.fill(tokenizer.decode(descriptions[0, i, :], skip_special_tokens=True), width=40)
 
-    img_emb = model.image_encoder(predicted_image_k)
-    txt_emb = model.text_encoder(pred_ids.unsqueeze(0))
+            ax[1, i].text(
+                0.5, 0.99,
+                wrapped_text,
+                ha='center',
+                va='top',
+                fontsize=10,
+                wrap=True
+            )
 
-    # if text encoder returns embrddinsg for each token. average them
-    if isinstance(txt_emb, tuple): #if text encoder returns tuple not tensor, take 1st value only
-      txt_emb = txt_emb[0]
-    if txt_emb.dim() == 3:
-      txt_emb = txt_emb.mean(dim=1)  # mean pool sequence
+            ax[1, i].axis('off')  # Hide axes for the text subplot
 
-    val_cross_modal = F.cosine_similarity(img_emb, txt_emb).mean().item()
-    print(f"Validation Cross-modal Similarity: {val_cross_modal:.4f}")
+        show_image(ax[0, 4], image_target[0].cpu())
+        ax[0, 4].set_title('Target')
+        ax[0, 4].set_aspect('auto')
+        ax[0, 4].axis('off')
+        text_target = text_target.squeeze(1)
 
-    figure, ax = plt.subplots(2, 6, figsize=(20, 5), gridspec_kw={'height_ratios': [2, 1.5]})
-
-    for i in range(4):
-      im = frames[0, i, :, :, :].cpu()
-      show_image(ax[0,i], im )
-      ax[0,i].set_aspect('auto')
-      ax[0,i].axis('off')
-      wrapped_text = textwrap.fill(tokenizer.decode(descriptions[0, i, :], skip_special_tokens=True), width=40)
-
-      ax[1,i].text(
-            0.5, 0.99,
-            wrapped_text,
-            ha='center',
-            va='top',
-            fontsize=10,
-            wrap=True
-        )
-
-      ax[1,i].axis('off') # Hide axes for the text subplot
-
-    show_image(ax[0,4], image_target[0].cpu())
-    ax[0,4].set_title('Target')
-    ax[0,4].set_aspect('auto')
-    ax[0,4].axis('off')
-    text_target = text_target.squeeze(1)
-
-    wrapped_text = textwrap.fill(tokenizer.decode(text_target[0], skip_special_tokens=True), width=40)
-    ax[1,4].text(
+        wrapped_text = textwrap.fill(tokenizer.decode(text_target[0], skip_special_tokens=True), width=40)
+        ax[1, 4].text(
             0.5, 0.99,
             wrapped_text,
             ha='center',
             va='top',
             fontsize=10,
             wrap=False)
-    ax[1,4].axis('off')
-    output = predicted_image_k[0, :, :, :].cpu() # this was context image i changed to content
-    show_image(ax[0,5], output)
-    ax[0,5].set_title('Predicted')
-    ax[0,5].set_aspect('auto')
-    ax[0,5].axis('off')
+        ax[1, 4].axis('off')
+        output = predicted_image_k[0, :, :, :].cpu()  # this was context image i changed to content
+        show_image(ax[0, 5], output)
+        ax[0, 5].set_title('Predicted')
+        ax[0, 5].set_aspect('auto')
+        ax[0, 5].axis('off')
 
-    generated_tokens = generate(model.text_decoder,
-                                hidden[:,0, :].unsqueeze(1),
-                                cell[:, 0, :].unsqueeze(1),
-                                max_len=150,
-                                sos_token_id=tokenizer.cls_token_id,
-                                eos_token_id=tokenizer.sep_token_id,
-                                device=device)
+        generated_tokens = generate(model.text_decoder,
+                                    hidden[:, 0, :].unsqueeze(1),
+                                    cell[:, 0, :].unsqueeze(1),
+                                    max_len=150,
+                                    sos_token_id=tokenizer.cls_token_id,
+                                    eos_token_id=tokenizer.sep_token_id,
+                                    device=device)
 
-    wrapped_text = textwrap.fill(tokenizer.decode(generated_tokens), width=40)
+        wrapped_text = textwrap.fill(tokenizer.decode(generated_tokens), width=40)
 
-    ax[1,5].text(
+        ax[1, 5].text(
             0.5, 0.99,
             wrapped_text,
             ha='center',
             va='top',
             fontsize=10,
-            wrap=False )
-    ax[1,5].axis('off')
-    plt.tight_layout()
-    plt.show()
-    return (
-    val_image_mse,
-    val_perplexity,
-    val_bleu,
-    val_cross_modal,
-    ssim_val
-)
+            wrap=False)
+        ax[1, 5].axis('off')
+        plt.tight_layout()
+        plt.show()
+        return (
+            val_image_mse,
+            val_perplexity,
+            val_bleu,
+            val_cross_modal,
+            ssim_val
+        )
 
-def show_image(ax, image, de_normalize = False, img_mean = None, img_std = None):
-  """
+
+def show_image(ax, image, de_normalize=False, img_mean=None, img_std=None):
+    """
   De-normalize the image (if necessary) and show image
   """
-  if de_normalize:
-    new_mean = -img_mean/img_std
-    new_std = 1/img_std
+    if de_normalize:
+        new_mean = -img_mean / img_std
+        new_std = 1 / img_std
 
-    image = transforms.Normalize(
-        mean=new_mean,
-        std=new_std
-    )(image)
-  ax.imshow(image.permute(1, 2, 0))
-
+        image = transforms.Normalize(
+            mean=new_mean,
+            std=new_std
+        )(image)
+    ax.imshow(image.permute(1, 2, 0))
