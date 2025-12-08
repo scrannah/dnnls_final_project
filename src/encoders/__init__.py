b533877431a66fa6f7f81ab77758c3d70f5bc9aa
scrannah <hannah.timms@hotmal.co.uk>
2025-12-08 14:00:16 +0000

    perceptual loss

--- src/encoders/__init__.py
+++ src/encoders/__init__.py
@@ -1,2 +1,3 @@
 from .visual_autoencoder import Backbone, VisualEncoder, VisualDecoder, VisualAutoencoder
 from .text_autoencoder import EncoderLSTM, DecoderLSTM, Seq2SeqLSTM
+from .perceptual_loss import PerceptualLoss
