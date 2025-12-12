c9dae0611db69e107da09f219d443298d5f761a5
scrannah <hannah.timms@hotmal.co.uk>
2025-12-12 16:05:29 +0000

    removing vae using perceptual loss instead

--- src/train/train_visual_autoencoder.py
+++ src/train/train_visual_autoencoder.py
@@ -11,7 +11,7 @@ def train_visual_autoencoder(
         kl_anneal_epoch,
         lambda_percep,
         device,
-        global_step
+        num_epochs=1
     ):
 
     epoch_losses = []
@@ -20,43 +20,44 @@ def train_visual_autoencoder(
 
     running_loss = 0.0
     running_kl = 0.0
+    for epoch in range(num_epochs):
 
-    for images in train_dataloader:
+        for images in train_dataloader:
 
-        # Move batch of images to device
-        images = images.to(device)
+            # Move batch of images to device
+            images = images.to(device)
 
-        # Forward pass (autoencoder returns reconstructed images)
-        x_content, x_context, mu, logvar = model(images)
+            # Forward pass (autoencoder returns reconstructed images)
+            x_content, x_context = model(images)
 
-        # Compute reconstruction loss, how does it compare to the actual image
-        loss = criterion_images(x_content, images)
-        ctxloss = criterion_ctx(x_context, images)
-        perceptual_loss = criterion_percep(x_content, images)
-        kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(),dim=1).mean()
+            # Compute reconstruction loss, how does it compare to the actual image
+            loss = criterion_images(x_content, images)
+            ctxloss = criterion_ctx(x_context, images)
+            perceptual_loss = criterion_percep(x_content, images)
+            # kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(),dim=1).mean()
 
-        kl_weight = min(beta, beta * global_step / kl_anneal_epoch)
+            # kl_weight = min(beta, beta * global_step / kl_anneal_epoch)
 
-        backprop_loss = loss + lambda_percep*perceptual_loss # + ctxloss kl_weight*kl_loss +
+            backprop_loss = loss + lambda_percep*perceptual_loss # + ctxloss
 
-        # Backpropagation
-        optimizer.zero_grad()
-        backprop_loss.backward()
-        optimizer.step()
+            # Backpropagation
+            optimizer.zero_grad()
+            backprop_loss.backward()
+            optimizer.step()
 
-        running_loss += backprop_loss.item() * images.size(0)
-        running_kl += kl_loss * images.size(0)
-
-        global_step += 1
-        #print(f"KL weight: {kl_weight}")
-        #print(f"Effective KL: {(kl_weight * kl_loss).item()}")
+            running_loss += backprop_loss.item() * images.size(0)
+            # running_kl += kl_loss * images.size(0)
 
 
-    epoch_loss = running_loss / len(train_dataloader.dataset)
-    # kl_loss = running_kl / len(train_dataloader.dataset)
-    epoch_losses.append(epoch_loss)
-    print(f" AE Loss: {epoch_loss:.4f}")
-    # print(f"Recon: {loss.item():.4f} | KL: {kl_loss.item():.4f}")
+            #print(f"KL weight: {kl_weight}")
+            #print(f"Effective KL: {(kl_weight * kl_loss).item()}")
 
 
-    return epoch_losses, global_step
+        epoch_loss = running_loss / len(train_dataloader.dataset)
+        # kl_loss = running_kl / len(train_dataloader.dataset)
+        epoch_losses.append(epoch_loss)
+        print(f" AE Loss: {epoch_loss:.4f}")
+        # print(f"Recon: {loss.item():.4f} | KL: {kl_loss.item():.4f}")
+
+
+    return epoch_losses
