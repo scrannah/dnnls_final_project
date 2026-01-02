b98dd46288de72b2f1a743a6b8a3e58aa36ef73b
scrannah <hannah.timms@hotmail.co.uk>
2026-01-02 16:51:27 +0000

    logging losses in visual ae

--- src/train/train_visual_autoencoder.py
+++ src/train/train_visual_autoencoder.py
@@ -14,13 +14,14 @@ def train_visual_autoencoder(
         num_epochs=1
 ):
     epoch_losses = []
-    epoch_perceploss = []
     model.train()
 
     for epoch in range(num_epochs):
 
         running_loss = 0.0
         running_percep = 0.0
+        running_ctx = 0.0 # CONTEXT IS MSE
+        running_l1 = 0.0 # CONTENT IS L1
 
         for images in train_dataloader:
             # Move batch of images to device
@@ -30,14 +31,14 @@ def train_visual_autoencoder(
             x_content, x_context = model(images)
 
             # Compute reconstruction loss, how does it compare to the actual image
-            loss = criterion_images(x_content, images)
+            l1loss = criterion_images(x_content, images)
             ctxloss = criterion_ctx(x_context, images)
             perceptual_loss = criterion_percep(x_content, images)
             # kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(),dim=1).mean()
 
             # kl_weight = min(beta, beta * global_step / kl_anneal_epoch)
 
-            backprop_loss = loss + lambda_percep * perceptual_loss + lambda_ctx * ctxloss
+            backprop_loss = l1loss + lambda_percep * perceptual_loss + lambda_ctx * ctxloss
 
             # Backpropagation
             optimizer.zero_grad()
@@ -46,18 +47,21 @@ def train_visual_autoencoder(
 
             running_loss += backprop_loss.item() * images.size(0)
             running_percep += perceptual_loss.item() * images.size(0)
+            running_l1 += l1loss.item() * images.size(0)
+            running_ctx += ctxloss.item() * images.size(0)
 
             # running_kl += kl_loss * images.size(0)
 
             # print(f"KL weight: {kl_weight}")
             # print(f"Effective KL: {(kl_weight * kl_loss).item()}")
 
-        epoch_loss = running_loss / len(train_dataloader.dataset)
-        # kl_loss = running_kl / len(train_dataloader.dataset)
-        epoch_losses.append(epoch_loss)
-        print(f"[Epoch {epoch + 1}]  AE combined Loss: {epoch_loss:.4f} MSE Loss: {loss:.4f} L1 Loss: {ctxloss:.4f} {loss:.4f} Perceptual Loss: {perceptual_loss:.4f}")
-        avg_percep = running_percep / len(train_dataloader.dataset)
+        combinedepoch_loss = running_loss / len(train_dataloader.dataset)
+        percep_lossavg = running_percep / len(train_dataloader.dataset)
+        l1_lossavg = running_l1 / len(train_dataloader.dataset)
+        ctx_lossavg = running_ctx / len(train_dataloader.dataset)
+        epoch_losses.append(combinedepoch_loss)
+        print(f"[Epoch {epoch + 1}]  AE combined Loss: {combinedepoch_loss:.4f} l1 (content) Loss: {l1_lossavg:.4f} mse (context) Loss: {ctx_lossavg:.4f} Perceptual Loss: {percep_lossavg:.4f}")
 
-        # print(f"Recon: {loss.item():.4f} | KL: {kl_loss.item():.4f}")
+
 
         return epoch_losses
