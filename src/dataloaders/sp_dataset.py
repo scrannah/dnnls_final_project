f128ff52d4caa4c69fed4e6f2fbce4005faf8ccb
scrannah <hannah.timms@hotmail.co.uk>
2026-01-13 18:05:12 +0000

    normalising

--- src/dataloaders/sp_dataset.py
+++ src/dataloaders/sp_dataset.py
@@ -14,8 +14,8 @@ class SequencePredictionDataset(Dataset):
         self.transform = transforms.Compose([
             transforms.Resize((60, 125)),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225]),  # HxWxC -> CxHxW
+            #transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 #std=[0.229, 0.224, 0.225]),  # HxWxC -> CxHxW
         ])
 
     def __len__(self):
